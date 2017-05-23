29de2c35883df040b5139e7e26fcce47d0ab7c5c
Kruzya <webmaster@kruzefag.ru>
2017-05-23 18:24:51 +0400

    Обновление вспомогательного скрипта для будущего обновления

---
+++ web_script.php
@@ -0,0 +1,50 @@
+<?php
+/**
+ * Init vars
+ */
+$engine     = isset($_GET['engine']) ? $_GET['engine'] : "orangebox";
+$volume     = isset($_GET['volume']) ? intval($_GET['volume']) : 100;
+$station    = isset($_GET['station']) ? $_GET['station'] : "";
+
+/**
+ * Small changes in vars.
+ */
+$volume = floatval($volume);
+
+/**
+ * Select content type.
+ */
+if ($_GET['engine'] == "old") {
+?><!-- Counter-Strike: Source v34 --><?php
+} else {
+?><!-- Orange Box / CSGO -->
+<audio id="hPlayer" autoplay>
+
+<script type="text/javascript">
+    var hAudioPlayer = document.getElementById("hPlayer");
+<?php if ($engine == "csgo") { ?>
+    hAudioPlayer.volume = <?= $volume; ?>;
+    hAudioPlayer.src    = <?= $station; ?>;
+<?php } else if ($engine == "orangebox") { ?>
+    var Volume = 1.0;
+
+    function ParseVolume(volume) {
+        return parseFloat(parseInt(volume) / 100);
+    }
+    window.onhashchange = function (event) {
+        location.hash.replace("#", "").split("&").forEach(function(Action, i, Actions) {
+            if (Action[0] == "V" && Action[5] == "e") {
+                Volume = ParseVolume(Action.replace("Volume=", ""));
+                hAudioPlayer.volume = Volume;
+            } else if (Action[0] == "S") {
+                src = Action.replace("Station=", "");
+                if (src != hAudioPlayer.src)
+                    hAudioPlayer.src = src;
+            }
+        });
+    }
+
+    window.onhashchange(null);
+<?php } ?>
+</script><?php
+}
